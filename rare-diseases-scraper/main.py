18ee381ac4a2e0c4cd05e095f188f574bc063c3f
Alex <hnguyen446@gatech.edu>
2024-02-10 02:50:31 -0500

    refactored web scraper

--- rare-diseases-scraper/main.py
+++ rare-diseases-scraper/main.py
@@ -4,40 +4,35 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from typing import List
 import time
+import json
 
 driver = webdriver.Chrome()
 driver.get("https://rarediseases.org/rare-diseases/?starts_with=A")
 
-scraper_txt = open("scraper-data.json", 'w').close() # clear current txt file
-
-scraper_txt = open("scraper-data.json", 'a')
-scraper_txt.write('{\n')
+data = {}
 
 def extract_page_data(driver):
     articles: List[WebElement] = driver.find_elements(By.XPATH, '//article')
     
     for article in articles:
-        
         anchor = article.find_element(By.XPATH, 'h5/a')
         aka = article.find_elements(By.XPATH, 'p/span')
         link = anchor.get_attribute('href')
         
-        scraper_txt.write(f'    "{anchor.text}" : {{\n')
+        print(anchor.text)
+        article_data = {
+            'aka': [nick.text for nick in aka],
+            'uri': link
+        }
+        data[anchor.text] = article_data
         
-        scraper_txt.write('        "aka": [\n')
-        
-        for nickname in aka:
-            scraper_txt.write(f'            "{nickname.text}",\n')
-        
-        scraper_txt.write('        ],\n')
-        scraper_txt.write(f'        "uri": "{link}",\n    }},\n\n')
-
     print("extracted page data")
 
 
 def extract_letter_data(driver):
     all_page_nums = driver.find_elements(By.XPATH, '//div[2]/div/div[2]/div[4]/div/ol/li')
     number_of_pages = 0
+
     if (len(all_page_nums) == 0):
         number_of_pages = 1
     else:
@@ -48,12 +43,16 @@ def extract_letter_data(driver):
         extract_page_data(driver)
         print(page_num)
 
+
 all_letters = driver.find_elements(By.XPATH, '//div[2]/div/div[2]/div[2]/a')
 
 for letter in range(len(all_letters)):
+    print(data)
     extract_letter_data(driver)
     print(letter)
-
-scraper_txt.write('}\n')
-scraper_txt.close()
+   
+   
+with open('scraper-data.json', 'w') as f:
+    json.dump(data, f, indent=4)
+         
 driver.close()
